7fb2f672a675922862eb856e77bece8deb0061ee
Mehmet Oktay <87772841+yEKr00tz@users.noreply.github.com>
2026-01-03 15:39:10 +0300

    Create TodoList.sol

---
+++ contracts/TodoList.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TodoList {
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    event TaskCreated(uint256 id, string content, bool completed);
+    event TaskCompleted(uint256 id, bool completed);
+
+    constructor() {
+        createTask("Blockchain Dunyasina Hos Geldiniz!");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint256 _id) public {
+        // 'memory' yerine 'storage' kullanmalıyız çünkü blockchain üzerindeki veriyi kalıcı değiştireceğiz
+        Task storage _task = tasks[_id]; 
+        _task.completed = !_task.completed;
+        emit TaskCompleted(_id, _task.completed);
+    }
+}
